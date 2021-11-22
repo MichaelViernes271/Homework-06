ca2dc21e7ebfe6e8b7214cbaa7ab40c34496ba15
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2021-11-22 12:47:39 +0800

    Some bug fix
    commit 340a3740216e20f2f0cd02b53c8b29cfe809a068
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Sat Nov 20 12:14:28 2021 +0800
    
                modified:   num-sorter.py
    
         1. The num sorter function works but still needs minor tweaks.

--- num-sorter.py
+++ num-sorter.py
@@ -15,3 +15,82 @@ def userinput():
     return numlist
 # End of function.
 
+def numsorter(sort_list):
+    try:
+        a, b, c, d = [i for i in sort_list]
+    except ValueError as e:
+        print("You entered incorrectly. Try again.")
+        
+    # Trying 24 combinations of a set of 4 elements.
+    
+    if a > b > c > d:
+        d, c, b, a = a, b, c, d
+    elif a > b > d > c:
+        d, c, b, a = a, b, d, c
+        print("sorted",a,b,c,d)
+    elif a > c > b > d:
+        d, c, b, a = a, c, b, d
+        print("sorted",a,b,c,d)
+    elif a > c > d > b:
+        d, c, b, a = a, c, d, b
+        print("sorted",a,b,c,d)
+    elif a > d > b > c:
+        d, c, b, a = a, d, b, c
+        print("sorted",a,b,c,d)
+    elif a > d > c > b:
+        d, c, b, a = a, d, c, b
+        print("sorted",a,b,c,d)
+    elif b > a > c > d:
+        d, c, b, a = b, a, c, d
+        print("sorted",a,b,c,d)
+    elif b > a > d > c:
+        d, c, b, a = b, a, d, c
+        print("sorted",a,b,c,d)
+    elif b > c > d > a:
+        d, c, b, a = b, c, d, a
+        print("sorted",a,b,c,d)
+    elif b > c > a > d:
+        d, c, b, a = b, c, a, d
+        print("sorted",a,b,c,d)
+    elif b > d > a > c:
+        d, c, b, a = b, d, a, c
+        print("sorted",a,b,c,d)
+    elif b > d > c > a:
+        d, c, b, a = b, d, c, a
+        print("sorted",a,b,c,d)
+    elif c > a > b > d:
+        d, c, b, a = c, a, b, d
+        print("sorted",a,b,c,d)
+    elif c > a > d > b:
+        d, c, b, a = c, a, d, b
+        print("sorted",a,b,c,d)
+    elif c > b > a > d:
+        d, c, b, a = c, b, a, d
+        print("sorted",a,b,c,d)
+    elif c > b > d > a:
+        d, c, b, a = c, b, d, a
+        print("sorted",a,b,c,d)
+    elif c > d > a > b:
+        d, c, b, a = c, d, a, b
+        print("sorted",a,b,c,d)
+    elif c > d > b > a:
+        d, c, b, a = c, d, b, a
+        print("sorted",a,b,c,d)
+    elif d > a > b > c:
+        d, c, b, a = d, a, b, c
+        print("sorted",a,b,c,d)
+    elif d > a > c > b:
+        d, c, b, a = d, a, c, b
+        print("sorted",a,b,c,d)
+    elif d > b > a > c:
+        d, c, b, a = d, b, a, c
+        print("sorted",a,b,c,d)
+    elif d > b > c > a:
+        d, c, b, a = d, b, c, a
+        print("sorted",a,b,c,d)
+    elif d > c > a > b:
+        d, c, b, a = d, c, a, b
+    else:
+        return True
+    return [a, b, c, d]
+# End of function.
